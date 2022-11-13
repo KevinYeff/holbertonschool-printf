4492ddd539be522f7a25153fc712f3157d3c1a3d
KevinYeff <kevinyeff94@gmail.com>
2022-11-12 22:14:43 -0500

    feat: C program that prints %.

---
+++ p_mdls.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * p_mdls -    Print special character.
+ * @list:      Attribute unused.
+ * Return:     1 if success.
+ */
+
+int p_mdls(va_lsit list __attribute__((unused)))
+{
+	_putchar('%');
+	return (1);
+}
